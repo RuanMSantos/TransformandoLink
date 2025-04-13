2b8d181eca6b7c2ac550b1f61bace078172fcc18
RuanMSantos <ruancauancauan@gmail.com>
2025-04-12 23:20:15 -0300

    Finaliza aplicação

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿int i = 0;
+string link = "https://youtu.be/GhN3iw0YLWQ?si=jGIVlPh44wPYNIgH";
+byte[] linkBinario = new byte[link.Length];
+char[] array = link.ToArray();
+
+foreach (char item in array){
+    linkBinario[i] = (byte)item;
+    i++;
+}
+
+string linkHexadecimal = Convert.ToHexString(linkBinario);
+string linkBase64 = Convert.ToBase64String(linkBinario);
+
+Console.WriteLine(linkHexadecimal);
+Console.WriteLine(linkBase64);
