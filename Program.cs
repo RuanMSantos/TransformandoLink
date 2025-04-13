957ac2d8ffa0abf59fc3bb45f5cc956aedacd5a6
RuanMSantos <ruancauancauan@gmail.com>
2025-04-13 01:43:04 -0300

    Adiciona adendo

--- Program.cs
+++ Program.cs
@@ -1,6 +1,6 @@
 ﻿string link = "https://youtu.be/GhN3iw0YLWQ?si=jGIVlPh44wPYNIgH";
-byte[] linkBinario = new byte[link.Length];
 char[] array = link.ToArray();
+byte[] linkBinario = new byte[link.Length];
 
 for (int i = 0; i < array.Length; i++){
     linkBinario[i] = (byte)array[i];
@@ -11,3 +11,6 @@ string linkBase64 = Convert.ToBase64String(linkBinario);
 
 Console.WriteLine(linkHexadecimal);
 Console.WriteLine(linkBase64);
+
+//byte[] linkBinario = Encoding.UTF8.GetBytes(link);
+// forma 15 vezes mais facil de fazer o que fiz da linha 3 à 7 
