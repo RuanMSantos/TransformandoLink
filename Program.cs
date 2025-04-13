0302612e617fc2e22ec55a2724e3bec57a05f310
RuanMSantos <ruancauancauan@gmail.com>
2025-04-13 02:29:36 -0300

    Adiciona sistema de entrada de dados

--- Program.cs
+++ Program.cs
@@ -1,16 +1,26 @@
-﻿string link = "https://youtu.be/GhN3iw0YLWQ?si=jGIVlPh44wPYNIgH";
-char[] array = link.ToArray();
-byte[] linkBinario = new byte[link.Length];
+﻿Console.Clear();
+Console.WriteLine("-- Conversor Hexadecimal e Base64 --\n");
 
-for (int i = 0; i < array.Length; i++){
-    linkBinario[i] = (byte)array[i];
+Console.Write("Escreva algo: ");
+string resposta = Console.ReadLine()!;
+
+char[] arrayResposta = resposta.ToArray();
+byte[] respostaByte = new byte[resposta.Length];
+
+for (int i = 0; i < arrayResposta.Length; i++){
+    respostaByte[i] = (byte)arrayResposta[i];
 }
 
-string linkHexadecimal = Convert.ToHexString(linkBinario);
-string linkBase64 = Convert.ToBase64String(linkBinario);
+string linkHexadecimal = Convert.ToHexString(respostaByte);
+string linkBase64 = Convert.ToBase64String(respostaByte);
 
+Console.WriteLine();
+Console.ForegroundColor = ConsoleColor.Magenta;
 Console.WriteLine(linkHexadecimal);
+Console.ForegroundColor = ConsoleColor.Yellow;
 Console.WriteLine(linkBase64);
+Console.ResetColor();
+Console.WriteLine();
 
-//byte[] linkBinario = Encoding.UTF8.GetBytes(link);
+// byte[] respostaByte = Encoding.UTF8.GetBytes(resposta);
 // forma 15 vezes mais facil de fazer o que fiz da linha 3 à 7 
